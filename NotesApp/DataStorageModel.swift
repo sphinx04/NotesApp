bc7825286e7211f3b84823aca87792bb0f71738f
Mnatsakan Zurnadzhian <mnats1997@gmail.com>
2023-03-09 13:50:00 +0400

    DataStorageModel update

--- NotesApp/DataStorageModel.swift
+++ NotesApp/DataStorageModel.swift
@@ -8,36 +8,6 @@
 import Foundation
 import SwiftUI
 
-class Data: ObservableObject {
-    @Published var model: DataStorageModel = DataStorageModel()
-    
-    func getDocumentsArray() -> [Document] {
-        objectWillChange.send()
-        return model.savedDocuments
-    }
-    
-    func addDocument(_ document: Document) {
-        model.savedDocuments.append(document)
-        objectWillChange.send()
-    }
-    func removeDocument(_ document: Document) {
-        model.savedDocuments.remove(at: model.savedDocuments.firstIndex(where: {$0 == document})!)
-        model.currentDocumentNumber = 0
-        objectWillChange.send()
-    }
-    
-    func setCurrentDocument(_ document: Document) {
-        model.currentDocumentNumber = model.savedDocuments.firstIndex(where: {$0 == document})!
-        objectWillChange.send()
-    }
-    
-    func resetAppStorage() {
-        model.savedDocuments = [Document(name: "test", text: "# Welcome test")]
-        model.currentDocumentNumber = 0
-    }
-    
-}
-
 struct DataStorageModel {
     @AppStorage("documents") var savedDocuments: [Document] = [Document(name: "test", text: "# Welcome test")]
     @AppStorage("currentDocumentNumber") var currentDocumentNumber: Int = 0
@@ -61,5 +31,21 @@ struct DataStorageModel {
     func resetAppStorage() {
         savedDocuments = [Document(name: "test", text: "# Welcome test")]
         currentDocumentNumber = 0
+    }
+    
+    func setCurrentName(_ name: String) {
+        savedDocuments[currentDocumentNumber].name = name
+    }
+    
+    func setCurrentText(_ text: String) {
+        savedDocuments[currentDocumentNumber].text = text
+    }
+    
+    func getCurrentName() -> String {
+        savedDocuments[currentDocumentNumber].name
+    }
+    
+    func getCurrentText() -> String {
+        savedDocuments[currentDocumentNumber].text
     }
 }
